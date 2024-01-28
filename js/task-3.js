b30e430e9fd67accb0b6dca968fa0987c2a6964b
a-nester <a_nester@ukr.net>
2024-01-29 00:10:02 +0200

    finish

--- js/task-3.js
+++ js/task-3.js
@@ -1,10 +1,18 @@
-function getElementWidth(content, padding, border) {
-    const a = Number.parseFloat(content);
-    const b = Number.parseFloat(padding);
-    const c = Number.parseFloat(border);
-    return a + 2 * b + 2 * c;
+function checkForSpam(message) {
+    formatMsg = message.toLowerCase()
+    if (formatMsg.includes("spam")) {
+        return formatMsg.includes("spam");
+    } else if (formatMsg.includes("sale")) {
+        return formatMsg.includes("sale");
+    } else {
+        return formatMsg.includes("sale");
+    }
 }
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
